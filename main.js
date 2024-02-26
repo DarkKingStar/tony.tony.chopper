f872aa2de0d633cc9135be2b23a7cab99d1da0ef
darkkingstar <sounakguha.12345@gmail.com>
2024-02-26 19:23:14 +0530

    repush

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const fastify = require("fastify")({ logger: false });
 const welcomeData = require("./welcome.js");
 const { getRoutes } = require("./route/getRoutes.js");
