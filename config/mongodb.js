7312a60a0e360424563bec5b83e7881bafd66286
darkkingstar <sounakguha.12345@gmail.com>
2024-02-27 01:07:20 +0530

    update: mongodb connection

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,25 +1,30 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
-
+/**
+ * Connects to MongoDB using the provided connection options.
+ * This code defines an async function connectToMongoDB that establishes a connection to a MongoDB database using the URL provided in the environment variables. It returns a Promise that resolves to the MongoDB database client. If the connection fails, it throws an error and logs the details.
+ * @return {Promise<Db>} A Promise that resolves to the MongoDB database client
+ */
 async function connectToMongoDB() {
+  const uri = process.env.MONGO_DB_URL;
+  if (!uri) {
+    throw new Error('MongoDB connection URL is not defined in environment variables');
+  }
+
   const connectionOptions = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    },
+    serverApi: ServerApiVersion.v1,
   };
 
-  const client = new MongoClient(process.env.MONGO_DB_URL, connectionOptions);
+  const client = new MongoClient(uri, connectionOptions);
 
   try {
     await client.connect();
     console.log('Connected to MongoDB');
     return client.db('chopperDB');
   } catch (err) {
-    console.error('Error connecting to MongoDB', err);
+    console.error('Error connecting to MongoDB:', err);
     throw err;
   }
 }
